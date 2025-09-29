097d485a0102d7bb71eb6083cbbb9b82b930a66a
Brenda Autolitano <brenautolitano@gmail.com>
2025-09-28 21:05:43 -0300

    User access roles and profiles

--- login-auth-system/app/initialize_db.py
+++
@@ -1,5 +0,0 @@
-# filepath: c:\Users\Brena\Desktop\PP3\PP3 Trabajo Final\login-auth-system\app\initialize_db.py
-from setup_db import setup_database
-
-if __name__ == '__main__':
-    setup_database()
